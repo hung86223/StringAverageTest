cecdc15661d0496882155b7f09d86119170ea3d5
ben.huang52 <ben.huang@titansoft.com.sg>
2019-09-16 22:14:06 +0800

    pass parsing test

--- StringAverageTest/StringFactory.cs
+++ StringAverageTest/StringFactory.cs
@@ -1,12 +1,51 @@
-﻿namespace StringAverageTest
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringAverageTest
 {
     public class StringFactory
     {
         private string _answer = "";
 
+        private readonly Dictionary<string, int> _stringToNumber = new Dictionary<string, int>()
+        {
+            {"zero",0},
+            {"one",1},
+            {"two",2},
+            {"three",3},
+            {"four",4},
+            {"five",5},
+            {"six",6},
+            {"seven",7},
+            {"eight",8},
+            {"nine",9}
+        };
+        private readonly Dictionary<int, string> _numberDictionary = new Dictionary<int, string>()
+        {
+            {0,"zero"},
+            {1,"one"},
+            {2,"two"},
+            {3,"three"},
+            {4,"four"},
+            {5,"five"},
+            {6,"six"},
+            {7,"seven"},
+            {8,"eight"},
+            {9,"nine"}
+        };
+
+
+        private int _numberAns;
+
         public string AverageString(string question)
         {
-            _answer = question;
+            var numbers = question.Split(' ');
+            foreach (var number in numbers)
+            {
+                _numberAns += _stringToNumber[number];
+            }
+
+            _answer = _numberDictionary[_numberAns];
             return QuestionIsEmpty(question) ? "N/A" :_answer ;
         }
 
